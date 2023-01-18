0e93d4df83002188ea066ccf09a7d8388212277e
andrew <andrey_budnyak@mail.ru>
2023-01-18 12:28:29 +0300

    Добавлены тестовые данные

--- Main-project/main.cpp
+++ Main-project/main.cpp
@@ -1,1 +1,13 @@
 //Budniak Andrew 3var 14gr
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #0. Library Subscription\n";
+    cout << "Author: Sergey Yermochenko\n";
+    cout << "Group: XX\n";
+    return 0;
+}
